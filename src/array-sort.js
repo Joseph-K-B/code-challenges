e33046aa710fedf52e947a05de2924f0b2c8a0a1
Joseph-K-B <303josephb@gmail.com>
2021-08-13 14:20:24 -0700

    passing lengthSort

--- src/array-sort.js
+++ src/array-sort.js
@@ -7,3 +7,7 @@ export function leastToGreatest(arr){
 export function greatestToLeast(arr){
     return arr.sort((x, y) => y - x);
   }
+
+  export function lengthSort(arr){
+    return arr.sort ((x, y) => x.length - y.length)
+}
--- src/array-sort.test.js
+++ src/array-sort.test.js
@@ -1,4 +1,4 @@
-import { leastToGreatest, greatestToLeast } from "./array-sort.js";
+import { leastToGreatest, greatestToLeast, lengthSort } from "./array-sort.js";
 
 test ('orders from least to greatest', ()=> {
     const input = [1, 3, 5, 2, 90, 20];
@@ -10,4 +10,10 @@ test ('orders from greatest to least', ()=> {
     const input = [1, 3, 5, 2, 90, 20];
     const output = greatestToLeast(input);
     expect(output).toEqual([90, 20, 5, 3, 2, 1])
+})
+
+test ('shortest to longest string', ()=> {
+    const input = ['dog', 'wolf', 'by', 'family', 'eaten']
+    const output = lengthSort(input);
+    expect(output).toEqual(['by', 'dog', 'wolf', 'eaten', 'family'])
 })
