f4fb87ff1ac5944d700b475c7df06d749f486d91
Joseph-K-B <303josephb@gmail.com>
2021-08-13 14:16:05 -0700

    greatestToLeast passing test

--- src/array-sort.js
+++ src/array-sort.js
@@ -3,3 +3,7 @@
 export function leastToGreatest(arr){
   return arr.sort((x, y) => x - y);
 }
+
+export function greatestToLeast(arr){
+    return arr.sort((x, y) => y - x);
+  }
--- src/array-sort.test.js
+++ src/array-sort.test.js
@@ -1,7 +1,13 @@
-import { leastToGreatest } from "./array-sort.js";
+import { leastToGreatest, greatestToLeast } from "./array-sort.js";
 
 test ('orders from least to greatest', ()=> {
     const input = [1, 3, 5, 2, 90, 20];
     const output = leastToGreatest(input);
     expect(output).toEqual([1, 2, 3, 5, 20, 90])
+})
+
+test ('orders from greatest to least', ()=> {
+    const input = [1, 3, 5, 2, 90, 20];
+    const output = greatestToLeast(input);
+    expect(output).toEqual([90, 20, 5, 3, 2, 1])
 })
