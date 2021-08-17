6585d5e68165d93106dbe4a0d3867d10bad30c58
Joseph-K-B <303josephb@gmail.com>
2021-08-17 07:32:46 -0700

    sortedKeys passing

--- src/object-keys-entries.js
+++ src/object-keys-entries.js
@@ -1,3 +1,7 @@
 export function capitalizeObjectKeys(obj) {
     return Object.keys(obj).map(obj => obj.toUpperCase());
 }
+
+export function sortedKeys(obj) {
+    return Object.keys(obj).sort((x, y) => x.length - y.length);
+}
